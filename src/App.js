e22deb4085f75d323753c4c139e749582c1f7e84
David Barcenas <daveepro@outlook.com>
2021-03-15 11:50:42 -0600

    react-refresh: Create Todo component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
+import Todo from './components/Todo';
+
 function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <div className="card">
-        <h2>TITLE</h2>
-        <div className="actions">
-          <button className="btn">Add</button>
-        </div>
-      </div>
+      <Todo title="Leran React" />
+      <Todo title="Master React" />
+      <Todo title="Explore the full React course" />
     </div>
   );
 }
---
+++ src/components/Todo.js
@@ -0,0 +1,12 @@
+function Todo({ title }) {
+  return (
+    <div className="card">
+      <h2>{title}</h2>
+      <div className="actions">
+        <button className="btn">Delete</button>
+      </div>
+    </div>
+  );
+}
+
+export default Todo;
