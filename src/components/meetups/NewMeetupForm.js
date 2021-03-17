d32d5cb92fc9a4338936a9e1675b4cbdb89eb0f7
David Barcenas <daveepro@outlook.com>
2021-03-16 19:24:27 -0600

    react-refresh: Create form to add a meetup

---
+++ src/components/meetups/NewMeetupForm.js
@@ -0,0 +1,30 @@
+import Card from '../ui/Card';
+import classes from './NewMeetupForm.module.css';
+
+export default function NewMeetupForm() {
+  return (
+    <Card>
+      <form className={classes.form}>
+        <div className={classes.control}>
+          <label htmlFor="title">Meetup Title</label>
+          <input type="text" required id="title" />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="image">Meetup Image</label>
+          <input type="url" required id="image" />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="address">Address</label>
+          <input type="text" required id="address" />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="description">Description</label>
+          <textarea id="description" cols="30" rows="5" required></textarea>
+        </div>
+        <div className={classes.actions}>
+          <button>Add Meetup</button>
+        </div>
+      </form>
+    </Card>
+  );
+}
--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,3 +1,10 @@
+import NewMeetupForm from '../components/meetups/NewMeetupForm';
+
 export default function NewMeetupsPage() {
-  return <div>New Meetups Page</div>;
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm />
+    </section>
+  );
 }
