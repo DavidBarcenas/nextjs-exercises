55b2d2980e53fa13a262ad002c2256d8cdfcc846
David Barcenas <daveepro@outlook.com>
2021-03-24 21:35:04 -0600

    routing: Extract dynamic param

--- routing/pages/portfolio/[projectId].js
+++ routing/pages/portfolio/[projectId].js
@@ -1,7 +1,11 @@
+import { useRouter } from 'next/router';
+
 function PortfolioProjectPage() {
+  const router = useRouter();
+  console.log(router.query);
   return (
     <div>
-      <h1>The Portfolio Project Page</h1>
+      <h1>The Portfolio Project Page </h1>
     </div>
   );
 }
