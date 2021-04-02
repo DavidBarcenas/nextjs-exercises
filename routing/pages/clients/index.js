1e876598f145b59df79ba0f08dfcdaded391800d
David Barcenas <daveepro@outlook.com>
2021-04-01 19:04:36 -0600

    routing: Navigate with Link component

--- routing/pages/clients/index.js
+++ routing/pages/clients/index.js
@@ -1,7 +1,22 @@
+import Link from 'next/link';
+
 function ClientsPage() {
+  const clients = [
+    { id: 'dave', name: 'Daveepro' },
+    { id: 'max', name: 'Maximilian' },
+    { id: 'manu', name: 'Manuel' },
+  ];
+
   return (
     <div>
       <h1>The Clients Page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
