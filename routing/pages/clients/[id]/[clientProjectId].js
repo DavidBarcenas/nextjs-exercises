d6eae272f758c40d7cf710c368cf362801a1b181
David Barcenas <daveepro@outlook.com>
2021-04-03 20:31:22 -0600

    routing-project: Start project time

--- routing/pages/clients/[id]/[clientProjectId].js
+++
@@ -1,9 +0,0 @@
-function ClientProjectIdPage() {
-  return (
-    <div>
-      <h1>The Clients Project ID Page</h1>
-    </div>
-  );
-}
-
-export default ClientProjectIdPage;
--- routing/pages/portfolio/list.js
+++
@@ -1,9 +0,0 @@
-function ListPage() {
-  return (
-    <div>
-      <h1>The List Page</h1>
-    </div>
-  );
-}
-
-export default ListPage;
