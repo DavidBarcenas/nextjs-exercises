a8dbfbb70df1f51f05b33c5606a4b23909108128
David Barcenas <daveepro@outlook.com>
2021-04-09 20:52:37 -0500

    routing-project: extract data in slug page

--- routing/pages/events/[...slug].js
+++ routing/pages/events/[...slug].js
@@ -1,9 +1,42 @@
+import { useRouter } from 'next/router';
+import { getFilteredEvents } from '../../dummy_data';
+import EventList from '../../components/events/event-list';
+
 function FilteredEventsPage() {
-  return (
-    <div>
-      <h1>Filtered Events Page</h1>
-    </div>
-  );
+  const router = useRouter();
+  const filterData = router.query.slug;
+
+  if (!filterData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p>Invalid filter. Please adjust your values!</p>;
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p>No event found for the chosen filter!</p>;
+  }
+
+  return <EventList items={filteredEvents} />;
 }
 
 export default FilteredEventsPage;
