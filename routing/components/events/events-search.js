fe31fb23ce7f4fb3598335fb55e9a2a39234698c
David Barcenas <daveepro@outlook.com>
2021-04-09 20:31:14 -0500

    routing-project: programmatically navigate to filter page

--- routing/components/events/events-search.js
+++ routing/components/events/events-search.js
@@ -1,20 +1,33 @@
+import { useRef } from 'react';
 import Button from '../ui/button';
 import classes from './events-search.module.css';
 
-function EventSearch() {
+function EventSearch({ onSearch }) {
+  const yearInputRef = useRef();
+  const monthInputRef = useRef();
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    const selectedYear = yearInputRef.current.value;
+    const selectedMonth = monthInputRef.current.value;
+
+    onSearch(selectedYear, selectedMonth);
+  }
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.controls}>
         <div className={classes.control}>
           <label htmlFor="year">Year</label>
-          <select name="year" id="year">
+          <select name="year" id="year" ref={yearInputRef}>
             <option value="2021">2021</option>
             <option value="2022">2022</option>
           </select>
         </div>
         <div className={classes.control}>
           <label htmlFor="month">Month</label>
-          <select name="month" id="month">
+          <select name="month" id="month" ref={monthInputRef}>
             <option value="1">January</option>
             <option value="2">February</option>
             <option value="3">March</option>
