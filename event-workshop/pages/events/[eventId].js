c0db054f00a4fa0ebb3478e8dfe8fce54743d651
David Barcenas <daveepro@outlook.com>
2021-10-04 10:54:21 -0500

    load data and paths for dynamic pages

--- event-workshop/pages/events/[eventId].js
+++ event-workshop/pages/events/[eventId].js
@@ -1,18 +1,12 @@
 import { Fragment } from 'react';
-import { useRouter } from 'next/router';
 
-import { getEventById } from '../../dummy-data';
+import { getAllEvents, getEventById } from '../../helpers/api-util';
 import EventSummary from '../../components/event-detail/event-summary';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventContent from '../../components/event-detail/event-content';
 import ErrorAlert from '../../components/ui/error-alert';
 
-function EventDetailPage() {
-  const router = useRouter();
-
-  const eventId = router.query.eventId;
-  const event = getEventById(eventId);
-
+function EventDetailPage({ event }) {
   if (!event) {
     return (
       <ErrorAlert>
@@ -35,6 +29,27 @@ function EventDetailPage() {
       </EventContent>
     </Fragment>
   );
+}
+
+export async function getStaticProps(context) {
+  const eventId = context.params.eventId;
+  const event = await getEventById(eventId);
+
+  return {
+    props: {
+      event
+    }
+  }
+}
+
+export async function getStaticPaths() {
+  const allEvents = await getAllEvents()
+  const paths = allEvents.map(e => ({ params: { eventId: e.id } }))
+
+  return {
+    paths,
+    fallback: false
+  }
 }
 
 export default EventDetailPage;
