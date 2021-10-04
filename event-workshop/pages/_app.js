aa47f24c6408a5bfe528ec56fe903dba3e9f31ac
David Barcenas <daveepro@outlook.com>
2021-10-04 17:32:48 -0500

    create general metadata in _app file and use _document file

--- event-workshop/pages/_app.js
+++ event-workshop/pages/_app.js
@@ -1,9 +1,21 @@
+import Head from "next/head";
+
 import Layout from '../components/layout/layout';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>NextJS Events</title>
+        <meta
+          name='description'
+          content='Find a lot of great events that alow you to envolve...'
+        />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
