2b1b4a6aa701613b811d38d53571a0c1a22a1b80
David Barcenas <daveepro@outlook.com>
2021-10-05 16:46:39 -0500

    send request to api routes

--- backend-api-routes/pages/index.js
+++ backend-api-routes/pages/index.js
@@ -1,8 +1,34 @@
+import { useRef } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const emailInputRef = useRef()
+  const feedbackInputRef = useRef()
+
+  function submitFormHandler(event) {
+    event.preventDefault()
+
+    const enteredEmail = emailInputRef.current.value
+    const enteredFeedback = feedbackInputRef.current.value
+
+    const reqBody = {
+      email: enteredEmail,
+      text: enteredFeedback
+    }
+
+    fetch('/api/feedback', {
+      method: 'POST',
+      body: JSON.stringify(reqBody),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then(data => console.log(data))
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -19,13 +45,13 @@ export default function Home() {
         <form>
           <div>
             <label htmlFor="email">Your Email Address</label>
-            <input type="email" id="email" />
+            <input type="email" id="email" ref={emailInputRef} />
           </div>
           <div>
             <label htmlFor="feedback">Your Feedback</label>
-            <textarea id="feedback" cols="30" rows="10"></textarea>
+            <textarea id="feedback" cols="30" rows="10" ref={feedbackInputRef}></textarea>
           </div>
-          <button>Send Feedback</button>
+          <button onClick={submitFormHandler}>Send Feedback</button>
         </form>
       </main>
 
