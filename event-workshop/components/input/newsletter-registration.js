68f31e94503b7c37d3d68f671d0e0e892150a285
David Barcenas <daveepro@outlook.com>
2021-10-07 17:35:42 -0500

    implement notifications to give feedback

--- event-workshop/components/input/newsletter-registration.js
+++ event-workshop/components/input/newsletter-registration.js
@@ -1,13 +1,22 @@
-import { useRef } from 'react';
+import { useContext, useRef } from 'react';
+
+import NotificationContext from '../../store/notification-context';
 import classes from './newsletter-registration.module.css';
 
 function NewsletterRegistration() {
+  const notificationCtx = useContext(NotificationContext)
   const emailInputRef = useRef()
 
   function registrationHandler(event) {
     event.preventDefault();
 
     const enteredEmail = emailInputRef.current.value
+
+    notificationCtx.showNotification({
+      title: 'Signing up...',
+      message: 'Registering for newsletter.',
+      status: 'pending'
+    })
 
     if (
       !enteredEmail ||
@@ -26,8 +35,29 @@ function NewsletterRegistration() {
         'Content-Type': 'application/json'
       }
     })
-      .then(response => response.json())
-      .then(data => console.log(data))
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+        }
+
+        return response.json().then(data => {
+          throw new Error(data.message || 'Something went wrong!')
+        })
+      })
+      .then(data => {
+        notificationCtx.showNotification({
+          title: 'Success!',
+          message: 'Successfully registered for newsletter!.',
+          status: 'success'
+        })
+      })
+      .catch(error => {
+        notificationCtx.showNotification({
+          title: 'Error!',
+          message: error.message || 'Something went wrong!',
+          status: 'error'
+        })
+      })
   }
 
   return (
