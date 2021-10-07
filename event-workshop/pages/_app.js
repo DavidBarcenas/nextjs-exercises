913e69acab6343877133516e705dc5b9f738f370
David Barcenas <daveepro@outlook.com>
2021-10-07 16:28:44 -0500

    add notification component

--- event-workshop/pages/_app.js
+++ event-workshop/pages/_app.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 
 import Layout from '../components/layout/layout';
+import Notification from "../components/ui/notification";
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
@@ -17,6 +18,7 @@ function MyApp({ Component, pageProps }) {
         />
       </Head>
       <Component {...pageProps} />
+      <Notification title='Test' message='This is a test.' status='success' />
     </Layout>
   );
 }
