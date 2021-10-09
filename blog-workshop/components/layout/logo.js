387ed133a24ef2cd994fa865dda9290930b6b403
David Barcenas <daveepro@outlook.com>
2021-10-08 20:42:15 -0500

    create layout and navigation component

---
+++ blog-workshop/components/layout/logo.js
@@ -0,0 +1,7 @@
+function Logo() {
+  return (
+    <h1>Logo</h1>
+  );
+}
+
+export default Logo;
---
+++ blog-workshop/components/layout/main-navigation.js
@@ -0,0 +1,23 @@
+import Link from 'next/link'
+
+import Logo from './logo';
+
+function MainNavigation() {
+  return (
+    <header>
+      <Link href="/">
+        <a>
+          <Logo />
+        </a>
+      </Link>
+      <nav>
+        <ul>
+          <li><Link href="/posts">Posts</Link></li>
+          <li><Link href="/contact">Contact</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default MainNavigation;
