807c8e1b724e552bfa3eecb452be07eac12c5576
David Barcenas <daveepro@outlook.com>
2021-10-08 19:41:58 -0500

    create the hero component layout

--- blog-workshop/components/home-page/hero.js
+++ blog-workshop/components/home-page/hero.js
@@ -1,6 +1,21 @@
+import Image from 'next/image'
+
+import classes from './hero.module.css'
+
 function Hero() {
   return (
-    <h1>Hero</h1>
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src='/images/site/user.jpeg'
+          alt='An image showing Davee'
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hi, I&apos;m Davee</h1>
+      <p>I blog web development - especially frontend frameworks like Angular or React.</p>
+    </section>
   );
 }
 
