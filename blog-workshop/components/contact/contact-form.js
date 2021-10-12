13c4dfbda036e9594a50673f88a88fb94314e44d
David Barcenas <daveepro@outlook.com>
2021-10-12 13:01:38 -0500

    create contact api route and send data from the client

--- blog-workshop/components/contact/contact-form.js
+++ blog-workshop/components/contact/contact-form.js
@@ -1,23 +1,69 @@
+import { useState } from 'react';
 import classes from './contact-form.module.css'
 
 function ContactForm() {
+  const [enteredEmail, setEnteredEmail] = useState('')
+  const [enteredName, setEnteredName] = useState('')
+  const [enteredMessage, setEnteredMessage] = useState('')
+
+  function sendMessageHandler(event) {
+    event.preventDefault()
+
+    if (
+      !enteredEmail || !enteredEmail.includes('@') ||
+      !enteredName || enteredName.trim() === '' ||
+      !enteredMessage || enteredMessage.trim() === ''
+    ) {
+      return;
+    }
+
+    fetch('/api/contact', {
+      method: 'POST',
+      body: JSON.stringify({
+        email: enteredEmail,
+        name: enteredName,
+        message: enteredMessage
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+  }
+
   return (
     <section className={classes.contact}>
       <h2>How can I help you?</h2>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={sendMessageHandler}>
         <div className={classes.controls}>
           <div className={classes.control}>
             <label htmlFor="email">Your Email</label>
-            <input type="email" id="email" required />
+            <input
+              type="email"
+              id="email"
+              required
+              value={enteredEmail}
+              onChange={e => setEnteredEmail(e.target.value)}
+            />
           </div>
           <div className={classes.control}>
             <label htmlFor="name">Your Name</label>
-            <input type="text" id="name" required />
+            <input
+              type="text"
+              id="name"
+              required
+              value={enteredName}
+              onChange={e => setEnteredName(e.target.value)}
+            />
           </div>
         </div>
         <div className={classes.control}>
           <label htmlFor="message">Your Message</label>
-          <textarea id="message" rows="5"></textarea>
+          <textarea
+            id="message"
+            rows="5"
+            value={enteredMessage}
+            onChange={e => setEnteredMessage(e.target.value)}
+          ></textarea>
         </div>
         <div className={classes.actions}>
           <button>Send Message</button>
