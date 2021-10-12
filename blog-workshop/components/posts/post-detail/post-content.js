b9a09341aee1f977cee365a0adea97d9f78438d0
David Barcenas <daveepro@outlook.com>
2021-10-11 19:58:21 -0500

    overwrite transform from markdown to html

--- blog-workshop/components/posts/post-detail/post-content.js
+++ blog-workshop/components/posts/post-detail/post-content.js
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import ReactMarkdown from 'react-markdown';
 
 import PostHeader from './post-header';
@@ -7,10 +8,33 @@ function PostContent(props) {
   const { slug, image, title, content } = props.post
   const imagePath = `/images/posts/${slug}/${image}`
 
+  const customComponents = {
+    p(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === 'img') {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${slug}/${image.properties.src}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    }
+  }
+
   return (
     <article className={classes.content}>
       <PostHeader title={title} image={imagePath} />
-      <ReactMarkdown>{content}</ReactMarkdown>
+      <ReactMarkdown components={customComponents}>{content}</ReactMarkdown>
     </article>
   );
 }
