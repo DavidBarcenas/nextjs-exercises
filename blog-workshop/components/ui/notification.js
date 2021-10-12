b96636d9e6800aeaa02279fbe2a2884fc23ffee7
David Barcenas <daveepro@outlook.com>
2021-10-12 15:14:01 -0500

    show notifications in react portal

--- blog-workshop/components/ui/notification.js
+++ blog-workshop/components/ui/notification.js
@@ -1,3 +1,5 @@
+import { ReactDOM } from 'react';
+
 import classes from './notification.module.css';
 
 function Notification(props) {
@@ -15,12 +17,12 @@ function Notification(props) {
 
   const cssClasses = `${classes.notification} ${statusClasses}`;
 
-  return (
+  return ReactDOM.createPortal((
     <div className={cssClasses}>
       <h2>{title}</h2>
       <p>{message}</p>
     </div>
-  );
+  ), document.getElementById('notifications'));
 }
 
 export default Notification;
