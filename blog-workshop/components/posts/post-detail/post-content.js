1cc89164b6a9002dc21499ef88ce0f9e8434411c
David Barcenas <daveepro@outlook.com>
2021-10-11 20:14:03 -0500

    render code snippets from markdown

--- blog-workshop/components/posts/post-detail/post-content.js
+++ blog-workshop/components/posts/post-detail/post-content.js
@@ -1,5 +1,7 @@
 import Image from 'next/image'
 import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 import PostHeader from './post-header';
 import classes from './post-content.module.css'
@@ -28,6 +30,19 @@ function PostContent(props) {
       }
 
       return <p>{paragraph.children}</p>;
+    },
+    code(code) {
+      const { className, children } = code;
+      const language = className.split('-')[1]; // className is something like language-js => We need the "js" part here
+
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          // eslint-disable-next-line react/no-children-prop
+          children={children}
+        />
+      );
     }
   }
 
