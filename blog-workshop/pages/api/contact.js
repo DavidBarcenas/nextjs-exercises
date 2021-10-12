2c3fb0ed8639ba8ace99814a4e631ff298a6fe57
David Barcenas <daveepro@outlook.com>
2021-10-12 13:53:09 -0500

    save message in database

--- blog-workshop/pages/api/contact.js
+++ blog-workshop/pages/api/contact.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb'
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const { email, name, message } = req.body
 
@@ -12,7 +14,29 @@ function handler(req, res) {
     }
 
     const newMessage = { email, name, message }
-    console.log(newMessage)
+
+    let client;
+
+    try {
+      client = await MongoClient.connect(process.env.MONGO_CONNECT)
+    } catch (error) {
+      res.status(500).json({ message: 'Could not connect to database.' })
+      return;
+    }
+
+    try {
+      const result = await client.db('nextjs')
+        .collection('messages')
+        .insertOne(newMessage)
+
+      newMessage._id = result.insertedId
+    } catch (error) {
+      client.close()
+      res.status(500).json({ message: 'Storing message failed!.' })
+      return;
+    }
+
+    client.close()
 
     res.status(201).json({
       message: 'Successfully stored message!',
