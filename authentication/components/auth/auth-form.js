3bbb8bc5634ee72a10f904866095ca1b43081f1e
David Barcenas <daveepro@outlook.com>
2021-10-13 13:59:18 -0500

    create user signup

--- authentication/components/auth/auth-form.js
+++ authentication/components/auth/auth-form.js
@@ -1,24 +1,61 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
+
 import classes from './auth-form.module.css';
+
+async function createUser(email, password) {
+  const response = await fetch('/api/auth/signup', {
+    method: 'POST',
+    body: JSON.stringify({ email, password }),
+    headers: { 'Content-Type': 'application/json' }
+  })
+
+  const data = await response.json()
+
+  if (!response.ok) {
+    throw new Error(data.message || 'Something went wrong!')
+  }
+
+  return data
+}
 
 export default function AuthForm() {
   const [isLogin, setIsLogin] = useState(true);
+  const emailRef = useRef()
+  const passwordRef = useRef()
 
   function switchAuthModeHandler() {
     setIsLogin((prevState) => !prevState);
   }
 
+  async function submitHandler(e) {
+    e.preventDefault()
+
+    const enteredEmail = emailRef.current.value
+    const enteredPassword = passwordRef.current.value
+
+    if (isLogin) {
+
+    } else {
+      try {
+        const result = await createUser(enteredEmail, enteredPassword)
+        console.log(result)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
+
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <input type='email' id='email' required ref={emailRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <input type='password' id='password' required ref={passwordRef} />
         </div>
         <div className={classes.actions}>
           <button>{isLogin ? 'Login' : 'Create Account'}</button>
--- authentication/pages/api/auth/signup.js
+++ authentication/pages/api/auth/signup.js
@@ -2,31 +2,33 @@ import { hashPassword } from "../../../helpers/auth";
 import { connectToDB } from "../../../helpers/db-utils"
 
 async function handler(req, res) {
-  const { email, password } = req.body
+  if (req.method === 'POST') {
+    const { email, password } = req.body
 
-  const invalidEmail = !email || !email.includes('@');
-  const invalidPassword = !password || password.trim().length < 7;
+    const invalidEmail = !email || !email.includes('@');
+    const invalidPassword = !password || password.trim().length < 7;
 
-  if (invalidEmail || invalidPassword) {
-    res.status(422).json({
-      message: 'Invalid input - password should also be at least 7 characters long.'
-    })
-    return
+    if (invalidEmail || invalidPassword) {
+      res.status(422).json({
+        message: 'Invalid input - password should also be at least 7 characters long.'
+      })
+      return
+    }
+
+    const hashedPassword = await hashPassword(password)
+
+    const client = await connectToDB()
+
+    await client
+      .db('nextjs')
+      .collection('users')
+      .insertOne({
+        email,
+        password: hashedPassword
+      })
+
+    res.status(201).json({ message: 'Created user!' })
   }
-
-  const hashedPassword = hashPassword(password)
-
-  const client = await connectToDB()
-
-  await client
-    .db('nextjs')
-    .collection('users')
-    .insertOne({
-      email,
-      password: hashedPassword
-    })
-
-  res.status(201).json({ message: 'Created user!' })
 }
 
 export default handler
