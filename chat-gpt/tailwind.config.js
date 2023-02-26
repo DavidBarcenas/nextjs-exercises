32fcab55a49e2d3072ca449aa7ef430ab377a7ee
David Barcenas <dbarcenas.dev@gmail.com>
2023-02-26 15:08:57 -0600

    chat-gpt: create layout

--- chat-gpt/tailwind.config.js
+++ chat-gpt/tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        typing: 'blink 1s steps(5, start) infinite'
+      },
+      keyframes: {
+        blink: {
+          to: { visibility: 'hidden' }
+        }
+      },
+      colors: {
+        gptlogo: '#10a37f',
+        gptdarkgray: '#202123',
+        gptgray: '#343541',
+        gptlightgray: '#444654'
+      }
+    }
   },
   plugins: [],
 }
