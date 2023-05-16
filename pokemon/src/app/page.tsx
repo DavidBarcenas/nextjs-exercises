2a55a73549a204b91c02504ce5b813a7137eb695
David Barcenas <dbarcenas.dev@gmail.com>
2023-05-15 22:09:33 -0600

    pokemon: get data in home

--- pokemon/src/app/page.tsx
+++ pokemon/src/app/page.tsx
@@ -1,5 +1,17 @@
-export default function Home() {
+async function getData() {
+  const res = await fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.json()
+}
+
+export default async function Home() {
+  const data = await getData()
+
   return (
-    <h1>Home</h1>
+    <h1>{data.name}</h1>
   )
 }
